8a65f68846cde329080dfed189de9ec6641ab9a4
deanxd <deanxd@foxmail.com>
2015-09-20 03:04:39 +0800

    实现控件的触摸事件的处理

--- Toggle/src/com/dean/toggle/view/ToggleView.java
+++ Toggle/src/com/dean/toggle/view/ToggleView.java
@@ -16,7 +16,7 @@ import android.view.View;
  * @comp Formssion
  * @date 2015-9-20
  * @desc ****
-
+ * 
  * @version $Rev$
  * @anthor $Author$
  * @Date $Date$
@@ -28,12 +28,21 @@ public class ToggleView extends View {
 	private Bitmap mBgBitmap;
 	private Bitmap mSlidingBitmap;
 	private Paint mPaint;
+	private boolean mIsOpen = false;
+	private boolean ACTION_UP = false;
+
+	private float mDownX;
+	private int mSlidWidth;
+	private int mBgWidth;
 
 	public ToggleView(Context context, AttributeSet attrs) {
 		super(context, attrs);
 		mPaint = new Paint();
 		setBitmap(R.drawable.switch_background,
 				R.drawable.slide_button_background);
+		mBgWidth = mBgBitmap.getWidth();
+		mSlidWidth = mSlidingBitmap.getWidth();
+		mDownX = mSlidWidth / 2;
 	}
 
 	public ToggleView(Context context) {
@@ -42,27 +51,55 @@ public class ToggleView extends View {
 
 	@Override
 	protected void onDraw(Canvas canvas) {
-
 		canvas.drawBitmap(mBgBitmap, 0, 0, mPaint);
-		canvas.drawBitmap(mSlidingBitmap, 0, 0, mPaint);
+		if (ACTION_UP) {
+			// 鼠标up
+			if(mIsOpen){
+				canvas.drawBitmap(mSlidingBitmap, mBgWidth - mSlidWidth, 0, mPaint);
+			}else{
+				canvas.drawBitmap(mSlidingBitmap, 0, 0, mPaint);
+			}
+		} else {
+			// 鼠标down or move
+			drawMoveOrDown(canvas);
+		}
+		ACTION_UP = false;
+	}
+
+	private void drawMoveOrDown(Canvas canvas) {
+		if (mDownX < mSlidWidth / 2) {
+			mDownX = mSlidWidth / 2;
+		}
+		if (mDownX > (mBgWidth - mSlidWidth / 2)) {
+			mDownX = mBgWidth - mSlidWidth / 2;
+		}
+		canvas.drawBitmap(mSlidingBitmap, mDownX - mSlidWidth / 2, 0, mPaint);
 	}
 
 	@Override
 	public boolean onTouchEvent(MotionEvent event) {
 		switch (event.getAction()) {
 		case MotionEvent.ACTION_DOWN:
-
+			mDownX = event.getX();
 			break;
 		case MotionEvent.ACTION_MOVE:
-
+			mDownX = event.getX();
 			break;
 		case MotionEvent.ACTION_UP:
-
+			mDownX = event.getX();
+			ACTION_UP = true;
+			if (mDownX < mBgWidth / 2 && mIsOpen) {
+					mIsOpen = false;
+			} else if (mDownX >= mBgWidth / 2 && !mIsOpen) {
+					mIsOpen = true;
+			}
 			break;
 		default:
 			break;
 		}
-		System.out.println("点击了");
+
+		// 重新绘制控件， 即重新执行onDraw()方法
+		invalidate();
 		return true;
 	}
 
